af25313084bc2152c61ce5e17a45a656f91081b5
Clint Cecil <umdmariachi@gmail.com>
2013-02-11 21:31:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webcomic::Application.config.secret_token = '709932cadf0bae288f17509255fde2b738cd3612b3e28377a7167eddb384badd5ef08985e45bbc8e2c6009dbcae69de75b61cc7e8a26f8036ca0a89df4b9e801'
