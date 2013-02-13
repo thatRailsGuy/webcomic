8a4b0898c4dc23343f0de1f03c8721423ed52add
Clint Cecil <umdmariachi@gmail.com>
2013-02-12 22:06:29 -0500

    added to views

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -12,8 +12,25 @@
 #  image_file_size    :integer
 #  image_updated_at   :datetime
 #
-
 class Comic < ActiveRecord::Base
   attr_accessible :description, :title, :image
-  has_attached_file :image
+  has_attached_file :image, 
+    :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100>" },
+    :default_style => :large
+  
+  def next
+    Comic.where("created_at > ?", created_at).order("created_at ASC").first
+  end
+  
+  def next?
+    !Comic.where("created_at > ?", created_at).order("created_at ASC").empty?
+  end
+  
+  def prev
+    Comic.where("created_at < ?", created_at).order("created_at ASC").last
+  end
+  
+  def prev?
+    !Comic.where("created_at < ?", created_at).order("created_at ASC").empty?
+  end
 end
