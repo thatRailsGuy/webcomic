5b59bfffcd7f03aba78429bf9c76efc16eb399f2
Justin Raines <justraines@gmail.com>
2013-02-13 11:39:04 -0500

    Got icons working!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,16 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to "#{title}".html_safe, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to "#{title} #{icon_sort(column)}".html_safe, {:sort => column, :direction => direction}, {:class => css_class}
   end
 
+  def icon_sort(column)
+    if sort_column == column
+      if sort_direction == "asc"
+        "<i class=\"iconic-arrow-up\"> </i>"
+      else
+        "<i class=\"iconic-arrow-down\"> </i>"
+      end
+    end
+  end
 end
